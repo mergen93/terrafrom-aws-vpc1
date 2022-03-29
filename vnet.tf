4e8a7804447b08d35b2fee53dadb53bd84015dd9
mergen93 <mergenjumadurdiyew@gmail.com>
2022-03-29 12:55:48 +0000

    hot fixed

--- vnet.tf
+++ vnet.tf
@@ -77,31 +77,27 @@ resource "azurerm_network_security_rule" "terraform" {
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.terraform.name
   vnet_location       = "East US"
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-}
-
-output "vnet_id" {
-    value = azurerm_virtual_network.terraform.id
-}
 
 
-output "subnet"{
-    value = azurerm_virtual_network.terraform.subnet
-}
+// output "vnet_id" {
+//     value = azurerm_virtual_network.terraform.id
+// }
 
 
-output "vnet_name"{
-    value = azurerm_virtual_network.terraform.name
-}
-output resource_group_name {
-  value = azurerm_resource_group.terraform.name
-}
+// output "subnet"{
+//     value = azurerm_virtual_network.terraform.subnet
+// }
+
+
+// output "vnet_name"{
+//     value = azurerm_virtual_network.terraform.name
+// }
+// output resource_group_name {
+//   value = azurerm_resource_group.terraform.name
+// }
